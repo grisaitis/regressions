d8b6e7951cdac6955ffecedac274838ad40a8cf7
William Grisaitis <wgrisaitis@gmail.com>
2015-01-01 22:26:05 -0500

    initial commit

---
+++ analyzeProstateData.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# todo: change this to use urllib2, instead of local file
+# info... http://stackoverflow.com/questions/1393324/in-python-given-a-url-to-a-text-file-what-is-the-simplest-way-to-read-the-cont
+
+df = pd.read_csv('prostate.data',sep='\t',index_col=0)
+
+print df.head()
+
+
