5ccbd826369c40208b985246e3f0a1e0f9845096
William Grisaitis <wgrisaitis@gmail.com>
2015-05-28 19:26:57 -0400

    adding latest stuff - forgot what went into it

--- prostate.py
+++ prostate.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 # todo: change this to use urllib2, instead of local file
 # info... http://stackoverflow.com/questions/1393324/in-python-given-a-url-to-a-text-file-what-is-the-simplest-way-to-read-the-cont
+# data source is http://statweb.stanford.edu/~tibs/ElemStatLearn/data.html
 
 df = pd.read_csv('prostate.data',sep='\t',index_col=0)
 
